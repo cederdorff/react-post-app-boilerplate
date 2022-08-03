9d842b0bb537ba1fc0897d9a40fb22aefa67c5fd
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-08-03 13:38:15 +0200

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,6 @@
 export default function HomePage() {
+    // in useEffect, fetch posts from: https://race-rest-default-rtdb.firebaseio.com/posts.json
+
     return (
         <section className="page">
             <h1>Posts Page</h1>
